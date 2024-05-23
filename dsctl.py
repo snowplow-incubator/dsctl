63198dd58729d57883c7a4035a4f7c1171293421
Jethro Nederhof <jethro.nederhof@snowplowanalytics.com>
2024-05-23 11:51:19 +1000

    Add pyproject.toml
    Make this repo installable via pip

--- dsctl.py
+++ dsctl.py
@@ -310,8 +310,7 @@ def flow(args: argparse.Namespace, config: Config) -> bool:
     else:
         return validate(config, schema, token, schema_type, args.includes_meta)
 
-
-if __name__ == "__main__":
+def main():
     arguments = parse_arguments()
     config = get_config()
 
@@ -327,4 +326,5 @@ if __name__ == "__main__":
         if not flow(arguments, config):
             sys.exit(1)
 
-    sys.exit(0)
+if __name__ == "__main__":
+    main()
